deec8b1b6c5c1b4918df55bd4cea93cfc012cc24
Gennaro Coppola <coppola612@gmail.com>
2022-04-16 13:10:40 +0300

    moved models in models.py

--- app.py
+++ app.py
@@ -17,46 +17,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = '6w33tOXubzT64MocVvg9Kg'
 
 db = SQLAlchemy(app)
+from models import *
 
 
-class CleverUsers(db.Model):
-    U_Id = db.Column(db.Integer, primary_key=True)
-    U_Name = db.Column(db.String(40), unique=False, nullable=False)
-    U_Username = db.Column(db.String(20), unique=False, nullable=False)
-    U_Password = db.Column(db.String(120), unique=False, nullable=False)
-    U_Email = db.Column(db.String(80), unique=False, nullable=False)
-    U_Score = db.Column(db.String(20), unique=False, nullable=True)
-    U_CharacterName = db.Column(db.String(20), unique=False, nullable=False)
-    U_Gender = db.Column(db.String(20), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<UserID %r>' % self.U_Id
-
-
-class GameInstance(db.Model):
-    GaI_Id = db.Column(db.Integer, primary_key=True)
-    GaI_user_id = db.Column(db.String(80), unique=False, nullable=False)
-    GaI_date = db.Column(db.String(120), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<UserID %r>' % self.GaI_user_id
-
-
-class Music(db.Model):
-    Mus_Id = db.Column(db.Integer, primary_key=True)
-    Mus_Name = db.Column(db.String(80), unique=False, nullable=False)
-    Mus_Lenght = db.Column(db.String(80), unique=False, nullable=False)
-    Mus_Artist = db.Column(db.String(80), unique=False, nullable=False)
-    Mus_Licence = db.Column(db.String(80), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<MusicId %r>' % self.Mus_Id
-
-
-db.create_all()
-
-
-# name, username, password, email, avatar, gender
 
 @app.route('/signup', methods=["POST", "GET"])
 def page_signup():
